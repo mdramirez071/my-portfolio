9777b9229db6a57404086468facf49ef955c1393
mdramirez071 <mdramirez071@gmail.com>
2019-07-25 15:14:32 -0700

    modifies index js file

--- server/index.js
+++ server/index.js
@@ -1,3 +1,3 @@
 const server = require("./server");
 const PORT = process.env.PORT || 3000;
-server.listen(PORT,() => console.log('server is listening on port 3000'));
+server.listen(PORT,'0.0.0.0',() => console.log('server is listening on port 3000'));
